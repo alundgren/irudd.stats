be49cd9aa9fdf65b1078ffd1a1b72a8eb9b09496
Andreas Lundgren <andreas.lundgren@naktergaltech.se>
2022-07-13 08:23:07 +0200

    Changed to controllers

---
+++ irudd.stats.host/irudd.stats.host/Controllers/HeartbeatController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Irudd.Stats
+{
+    [Route("api/hb")]
+    [ApiController]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public class HeartbeatController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Heartbeat()
+        {
+            return Ok(new { Date = DateTime.UtcNow.ToString("o") });
+        }
+    }
+}
--- irudd.stats.host/irudd.stats.host/Program.cs
+++ irudd.stats.host/irudd.stats.host/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -16,28 +17,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.UseRouting();
+app.UseEndpoints(endpoints =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    endpoints.MapControllers();
+});
 
 app.Run();
-
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
